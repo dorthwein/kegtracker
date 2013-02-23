4498da3a3a4580249465d366d8c34fe551965844
Danny Orthwein <dorthwei@unknown18593348bb30.gateway.2wire.net>
2013-02-23 12:39:07 -0600

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,8 @@ class HomeController < ApplicationController
 	  if user_signed_in?
 		  format.html { redirect_to :dashboard_viewer }    	  			
 	  else
-	 	format.html { redirect_to new_user_session_path, notice: 'Please Login.' } 
-#		  format.html { redirect_to public_home_path, notice: 'Please Login.' } 
+#	 	format.html { redirect_to new_user_session_path, notice: 'Please Login.' } 
+		format.html { redirect_to public_home_path, notice: 'Please Login.' } 
 	  end
     end
   end
