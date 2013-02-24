8a4784aa4d2e3fdd4da74d082ed520f0068ec882
Danny Orthwein <dorthwei@unknown18593348bb30.gateway.2wire.net>
2013-02-23 23:31:42 -0600

    update

--- app/controllers/reports/asset_controller.rb
+++ app/controllers/reports/asset_controller.rb
@@ -38,48 +38,48 @@ class Reports::AssetController < ApplicationController
 			format.json {
 				gatherer = Gatherer.new(current_user.entity)
 				location, product, entity = gatherer.asset_activity_fact_criteria				
-				transactions = '' 
+				transactions = 'No Fill Event Found' 
 				response = {}
 
 				transaction_facts = AssetActivityFact.where(:fill_asset_activity_fact_id => params[:fill_asset_activity_fact_id]).any_of(location, product, entity).desc(:fact_time)
 				x = transaction_facts.first							
+				if !x.nil?
+		            x_product = 	"<td> <b> 	Product: 	</b> 	#{x.product.description}	 </td>"
+		            x_brewery = 	"<td> <b> 	Brewery: 	</b> 	#{x.product.entity.description} </td>"
+		            x_asset_type = 	"<td> <b> 	Asset Type: </b> 	#{x.asset_type.description} </td>"
+		            x_entity = 		"<td> <b> 	Owner: 		</b> 	#{x.entity.description} </td>"
+		            x_fill_date = 	"<td> <b> 	Fill Date: 	</b> 	#{x.fact_time.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")} </td>"
 
-	            x_product = 	"<td> <b> 	Product: 	</b> 	#{x.product.description}	 </td>"
-	            x_brewery = 	"<td> <b> 	Brewery: 	</b> 	#{x.product.entity.description} </td>"
-	            x_asset_type = 	"<td> <b> 	Asset Type: </b> 	#{x.asset_type.description} </td>"
-	            x_entity = 		"<td> <b> 	Owner: 		</b> 	#{x.entity.description} </td>"
-	            x_fill_date = 	"<td> <b> 	Fill Date: 	</b> 	#{x.fact_time.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")} </td>"
+					x_fill_count = 	"<td> <b> 	Fill Count: </b> 	#{x.fill_count} 		</td>"
+					x_tag_value = 	"<td> <b> 	Tag Value: 	</b> 	#{x.asset.tag_value} 	</td>"
+					x_tag_key = 	"<td> <b> 	Tag Key: 	</b> 	#{x.asset.tag_key} 		</td>"
 
-				x_fill_count = 	"<td> <b> 	Fill Count: </b> 	#{x.fill_count} 		</td>"
-				x_tag_value = 	"<td> <b> 	Tag Value: 	</b> 	#{x.asset.tag_value} 	</td>"
-				x_tag_key = 	"<td> <b> 	Tag Key: 	</b> 	#{x.asset.tag_key} 		</td>"
+					# Start Table
+					transactions = "<table> <tbody> " 
 
-				# Start Table
-				transactions = "<table> <tbody> " 
-
-				# Add Asset Details
-				transactions = transactions + "<tr>" + x_product + x_brewery + x_asset_type + x_entity + x_fill_date + " </tr> "
-				transactions = transactions + "<tr>" + x_fill_count + x_tag_value + x_tag_key + "<td></td><td></td> </tr> "
-				# Activity Header
-				transactions = transactions + ' <tr> <td colspan="5"> <h3 style="border-bottom:1px solid #CCC;border-top:1px solid #CCC; margin:10px 0 0 0;"> Activity </h3> </td> </tr>'
-
-
-				
-				transactions = transactions + '<tr>	<th> Action	</th> <th> Location </th> <th> Location Network	</th> <th> Date </th> <th> Transaction Entity </th> </tr>'
+					# Add Asset Details
+					transactions = transactions + "<tr>" + x_product + x_brewery + x_asset_type + x_entity + x_fill_date + " </tr> "
+					transactions = transactions + "<tr>" + x_fill_count + x_tag_value + x_tag_key + "<td></td><td></td> </tr> "
+					# Activity Header
+					transactions = transactions + ' <tr> <td colspan="5"> <h3 style="border-bottom:1px solid #CCC;border-top:1px solid #CCC; margin:10px 0 0 0;"> Activity </h3> </td> </tr>'
 
 
-				# For each transaction, create a row
-				transaction_facts.each do |x|					
-					action = 					"<td> #{x.handle_code_description} </td>"
-					location = 					"<td> #{x.location.description} </td>"
-					location_network = 			"<td> #{x.location.network_description} </td>"
-					date = 						"<td> #{x.fact_time.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")} </td>"
-					transaction_entity = 		"<td> #{x.user.entity.description} </td>"
-					fill_asset_activity_fact = 	x.fill_asset_activity_fact_id
+					
+					transactions = transactions + '<tr>	<th> Action	</th> <th> Location </th> <th> Location Network	</th> <th> Date </th> <th> Transaction Entity </th> </tr>'
 
-					transactions = transactions + '<tr>' + action + location + location_network + date + transaction_entity +  '</tr>'
+
+					# For each transaction, create a row
+					transaction_facts.each do |x|					
+						action = 					"<td> #{x.handle_code_description} </td>"
+						location = 					"<td> #{x.location.description} </td>"
+						location_network = 			"<td> #{x.location.network_description} </td>"
+						date = 						"<td> #{x.fact_time.in_time_zone("Central Time (US & Canada)").strftime("%b %d, %Y")} </td>"
+						transaction_entity = 		"<td> #{x.user.entity.description} </td>"
+						fill_asset_activity_fact = 	x.fill_asset_activity_fact_id
+
+						transactions = transactions + '<tr>' + action + location + location_network + date + transaction_entity +  '</tr>'
+					end
 				end
-
 				response = {:html => transactions}
 				render json: response
 			}
