a33cc21bae6152ad568e55bc8033e449eac5172e
Danny Orthwein <dorthwei@DFO-2.local>
2013-02-28 13:50:29 -0600

    update

--- config/initializers/speedycore.rb
+++ config/initializers/speedycore.rb
@@ -1,9 +1,4 @@
 # config/initializers/spreedly.rb
-
-if Rails.env.development?  
 	SpreedlyCore.configure(:api_login => 'ZCqlTGZYtZQesf5Zodny9jtQwj3', :api_secret => 'Pmrm06fqq4pGrgF3a83OFq1oB6boINInoxtV6pC5mvx7fj0P6KUm0iPLDtFoqv4J' )
-else Rails.env.production?  
-	SpreedlyCore.configure
-end
 #SPREEDLYCORE_API_LOGIN:     
 #SPREEDLYCORE_API_SECRET:    
