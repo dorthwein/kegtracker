bd720e057ddb815fee02361b5fc6679ff3a50566
Danny Orthwein <dorthwei@DFO-2.local>
2013-04-12 12:42:02 -0500

    update

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -4,9 +4,17 @@ class AccountController < ApplicationController
 
 	def index
 		if !params[:token].nil?
-			token = params[:token]			
-			payment_method = SpreedlyCore::PaymentMethod.find(token) # Not Working...
+			payment_token = params[:token]			
+			payment_method = SpreedlyCore::PaymentMethod.find(payment_token)
+			if payment_method.valid?
+			  purchase_transaction = payment_method.purchase(550)
+			  purchase_transaction.succeeded? # true
+			else
+			  flash[:notice] = "Woops!\n" + payment_method.errors.join("\n")
+			end
 
+=begin
+			print payment_method.to_s
 			if payment_method.valid?
 				payment_method.retain # Keep This
 				current_user.entity.payment_token = token
@@ -15,10 +23,11 @@ class AccountController < ApplicationController
 				# Move This
 				purchase_transaction = payment_method.purchase(550)
 				print purchase_transaction.succeeded? # true
-			 	print 'fuck'
+			 	print 'IT WORKED'
 			else
 			  print "Woops!\n" + payment_method.errors.join("\n")
 			end
+=end			
 		end		
 
 		respond_to do |format|
