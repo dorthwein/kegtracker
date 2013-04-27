0273e62af459c364b5089bc4bf88cc874db512c9
Danny Orthwein <dorthwei@dfo.gateway.2wire.net>
2013-04-27 14:28:14 -0500

    update

--- app/controllers/public/join_controller.rb
+++ app/controllers/public/join_controller.rb
@@ -14,9 +14,11 @@ class Public::JoinController < ApplicationController
 				user = User.where(:email => params[:user][:email]).first
 				if user.nil?
 					user = User.create!(params[:user])
+					params[:entity][:mode] = 1
 					entity = Entity.create!(params[:entity])
 					user.entity_id = entity._id
 					entity.keg_tracker = 1
+					
 				end
 
 				if user.save! && entity.save!
